24a8a01a7e306866aa7146fd80edb457400c96d1
ascor <ascor@Archbox42>
2018-11-07 12:14:22 +0000

    # WARNING: head commit changed in the meantime
    Still not sure how this works.

--- prog1git/src/Application1.java
+++ prog1git/src/Application1.java
@@ -3,6 +3,6 @@ public class Application1
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Willkommen zu Programmieren1 im Wintersemester 2018");
+		System.out.println("Willkommen zu Programmieren1 im Wintersemester 2018.");
 	}
 }
