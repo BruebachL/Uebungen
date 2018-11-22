3f02fe54f9c68119d1da8aaf9d86e7e48359b862
Lukas Bruebach <lukas.bruebach@student.fhws.de>
2018-11-22 10:08:54 +0000

    Rekurive Potenz

--- prog1git/src/Rekursion.java
+++ prog1git/src/Rekursion.java
@@ -1,4 +1,27 @@
 package prog1git.src;
 
+import java.util.Scanner;
+
 public class Rekursion {
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bitte geben Sie die Basis an.");
+        int basis = scanner.nextInt();
+        System.out.println("Bitte geben Sie den Exponent an.");
+        int exponent = scanner.nextInt();
+        System.out.println("Das Ergebnis betraegt:");
+        System.out.println(potenz(basis, exponent));
+    }
+    private static int potenz(int basis, int exponent)
+    {
+        if(exponent==0)
+        {
+            return 1;
+        }
+        else{
+            return basis * potenz(basis, exponent-1);
+        }
+
+    }
 }
