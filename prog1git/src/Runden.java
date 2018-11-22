203862a847b2f5c4bad700fb4528851dc39c91d5
Lukas Bruebach <lukas.bruebach@student.fhws.de>
2018-11-22 12:19:03 +0000

    Rundungsfunktion rundet nun negative Zahlen korrekt.

--- prog1git/src/Runden.java
+++ prog1git/src/Runden.java
@@ -2,7 +2,6 @@ package prog1git.src;
 
 import java.util.Scanner;
 
-// TODO: Negative Zahlen erkennen und korrekt ausgeben.
 public class Runden {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -10,26 +9,44 @@ public class Runden {
         double zahl = scanner.nextDouble();
         System.out.println("Bitte geben Sie die Anzahl der Nachkommastellen an.");
         int nachkommastellen = scanner.nextInt();
-        double gerundeteZahl = runden(zahl, nachkommastellen);
-        System.out.println("Die gerundete Zahl betraegt: \n" + gerundeteZahl);
+        System.out.println("Die gerundete Zahl betraegt: \n" + runden(zahl, nachkommastellen));
     }
     private static double runden(double zahl, int nachkommastellen) {
         if (nachkommastellen == 0) {
-            String[] splitter = Double.toString(zahl).split("\\.");
-            while (splitter[1].length() != 1) {
-                double nachkommaziffern = Double.parseDouble(splitter[1]);
-                if (nachkommaziffern % 10 <= 4) {
-                    nachkommaziffern = (nachkommaziffern - nachkommaziffern % 10) / 10;
-                } else {
-                    nachkommaziffern = ((nachkommaziffern - nachkommaziffern % 10) / 10) + 1;
+            if(zahl>0) {
+                String[] splitter = Double.toString(zahl).split("\\.");
+                while (splitter[1].length() != 1) {
+                    double nachkommaziffern = Double.parseDouble(splitter[1]);
+                    if (nachkommaziffern % 10 <= 4) {
+                        nachkommaziffern = (nachkommaziffern - nachkommaziffern % 10) / 10;
+                    } else {
+                        nachkommaziffern = ((nachkommaziffern - nachkommaziffern % 10) / 10) + 1;
+                    }
+                    splitter[1] = Integer.toString((int) nachkommaziffern);
                 }
-                splitter[1] = Integer.toString((int) nachkommaziffern);
-            }
-            int ersteNachkommaziffer = Integer.parseInt(splitter[1]);
-            if (ersteNachkommaziffer<=4) {
-                return Integer.parseInt(splitter[0]);
-            }else {
-                return Integer.parseInt(splitter[0]) + 1;
+                int ersteNachkommaziffer = Integer.parseInt(splitter[1]);
+                if (ersteNachkommaziffer <= 4) {
+                    return Integer.parseInt(splitter[0]);
+                } else {
+                    return Integer.parseInt(splitter[0]) + 1;
+                }
+            }else{
+                String[] splitter = Double.toString(zahl).split("\\.");
+                while (splitter[1].length() != 1) {
+                    double nachkommaziffern = Double.parseDouble(splitter[1]);
+                    if (nachkommaziffern % 10 <= 4) {
+                        nachkommaziffern = (nachkommaziffern - nachkommaziffern % 10) / 10;
+                    } else {
+                        nachkommaziffern = ((nachkommaziffern - nachkommaziffern % 10) / 10) + 1;
+                    }
+                    splitter[1] = Integer.toString((int) nachkommaziffern);
+                }
+                int ersteNachkommaziffer = Integer.parseInt(splitter[1]);
+                if (ersteNachkommaziffer <= 4) {
+                    return Integer.parseInt(splitter[0]);
+                } else {
+                    return Integer.parseInt(splitter[0]) - 1;
+                }
             }
         } else {
             String[] splitter = Double.toString(zahl).split("\\.");
