a8834c2aeebf47318d6e15c6d2507704043f2d99
Ascor <lukas.bruebach@student.fhws.de>
2018-12-09 17:38:10 +0100

    MinMax und Lotto

--- prog1git/src/Lotto.java
+++ prog1git/src/Lotto.java
@@ -1,10 +1,74 @@
+import java.util.Scanner;
+
 public class Lotto {
     public static void main(String[] args) {
-        int[] ziehung = ziehung(6);
+        System.out.print("Bitte geben Sie die Anzahl der zu ziehenden Zahlen an: ");
+        Scanner scanner = new Scanner(System.in);
+        int anzahl = scanner.nextInt();
+        int[] ziehung = ziehung(anzahl);
+        int[] tip = createTip(anzahl);
+        System.out.println("Die heutige Ziehung ist:");
+        printZiehung(sortZiehung(ziehung, anzahl));
+        System.out.println("Ihre Lottozahlen waren:");
+        printZiehung(sortZiehung(tip, anzahl));
+        if(compareHits(ziehung, tip) == 0){
+            System.out.println("Sie haben leider keine richtigen Zahlen getippt.");
+        }else {
+            System.out.println("Sie haben " + compareHits(ziehung, tip) + " richtige getippt:");
+            printZiehung(stashHits(ziehung, tip, compareHits(ziehung, tip)));
+        }
+        scanner.close();
+
+    }
+    private static int[] stashHits (int[] ziehung, int[] tip, int hits){
+        int[] correctGuesses = new int[hits];
+        int guessPosition = 0;
+        for(int i = 0; i<ziehung.length; i++){
+            for (int cnt = 0; cnt<tip.length; cnt++){
+                if (tip[i]==ziehung[cnt]){
+                    correctGuesses[guessPosition] = tip[i];
+                    guessPosition++;
+                }
+            }
+        }
+        return correctGuesses;
+    }
+    private static int compareHits (int[] ziehung, int[] tip){
+        int hits = 0;
+        for(int i = 0; i<ziehung.length; i++){
+            for (int cnt = 0; cnt<tip.length; cnt++){
+                if (tip[i]==ziehung[cnt]){
+                    hits++;
+                }
+            }
+        }
+        return hits;
+    }
+    private static int[] createTip(int ziehungsGröße){
+        int[] tip = new int[ziehungsGröße];
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < ziehungsGröße; i++)
+        {
+            System.out.print("Ihr " + (i + 1) + ". Tip: ");
+            tip[i] = scanner.nextInt();
+            boolean correctTip = false;
+            while (!correctTip){
+            if(tip[i] > 0 && tip[i] < 50) {
+                correctTip = true;
+            }else{
+                System.out.println("Bitte geben Sie eine Zahl zwischen 1 und 49 an.");
+                tip[i] = scanner.nextInt();
+            }
+        }
+        }
+        scanner.close();
+        return tip;
+
+    }
+    private static int[] sortZiehung(int[] ziehung, int anzahl){
         int temp = 0;
-        boolean keineDoppelung = false;
-        for (int cnt = 0; cnt <= 5; cnt++) {
-            for (int i = cnt; i <= 5; i++) {
+        for (int cnt = 0; cnt <= anzahl-1; cnt++) {
+            for (int i = cnt; i <= anzahl-1; i++) {
                 if (ziehung[cnt] > ziehung[i]) {
                     temp = ziehung[cnt];
                     ziehung[cnt] = ziehung[i];
@@ -12,32 +76,29 @@ public class Lotto {
                 }
             }
         }
-        while(keineDoppelung == false) {
-            for (int i = 0; i <= 4; i++) {
-                if (ziehung[i] == ziehung[i + 1]) {
-                    ziehung = ziehung(6);
-                    i = 0;
-                }
-            }
-            keineDoppelung = true;
-        }
-        for (int cnt = 0; cnt <= 5; cnt++) {
-            for (int i = cnt; i <= 5; i++) {
-                if (ziehung[cnt] > ziehung[i]) {
-                    temp = ziehung[cnt];
-                    ziehung[cnt] = ziehung[i];
-                    ziehung[i] = temp;
-                }
-            }
-        }
+        return ziehung;
+    }
+    private static void printZiehung (int[] ziehung){
         for (int i = 0; i < ziehung.length; i++) {
-            System.out.println(ziehung[i]);
+            if (i==ziehung.length-1){
+                System.out.println(ziehung[i] + ". ");
+            }else {
+                System.out.print(ziehung[i] + ", ");
+            }
         }
     }
     private static int[] ziehung(int anzahl){
         int[] ziehung = new int[anzahl];
-        for (int i = 0; i <= 5; i++) {
+        for (int i = 0; i <= anzahl-1; i++) {
+
             ziehung[i] = (int) (Math.random() * 49 + 1);
+            for (int cnt = 0; cnt < i; cnt++){
+                if (ziehung[cnt]==ziehung[i])
+                {
+                    ziehung[i] = (int) (Math.random() * 49 + 1);
+                    cnt = 0;
+                }
+            }
         }
         return ziehung;
     }
---
+++ prog1git/src/MittelMinMax.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class MittelMinMax {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bitte geben Sie eine Tabellengröße an: ");
+        int tableSize = scanner.nextInt();
+        double[] table = sortTable(createTable(tableSize), tableSize);
+        System.out.println("Kleinster Wert: " + table[0]);
+        System.out.println("Größter Wert: "+ table[tableSize-1]);
+        System.out.println("Arithmetischer Mittel: " + arithmeticMiddle(table, tableSize));
+
+    }
+    private static double arithmeticMiddle(double[] table, int tableSize){
+        double result = 0;
+        for(double i = 0; i < tableSize; i++){
+            result += table[(int)i];
+        }
+        return result/tableSize;
+    }
+    private static double[] createTable(int tableSize){
+        double[] table = new double[tableSize];
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < tableSize; i++)
+        {
+            System.out.print((i + 1) + ". Wert: ");
+            table[i] = scanner.nextDouble();
+        }
+        return table;
+    }
+    private static double[] sortTable(double[] table, int tableSize){
+        double temp = 0;
+        for (int cnt = 0; cnt <= tableSize-1; cnt++) {
+            for (int i = cnt; i <= tableSize-1; i++) {
+                if (table[cnt] > table[i]) {
+                    temp = table[cnt];
+                    table[cnt] = table[i];
+                    table[i] = temp;
+                }
+            }
+        }
+        return table;
+    }
+}
