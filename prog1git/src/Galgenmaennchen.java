c855c813a15a66f0695da4e8baf512483eed55ca
Ascor <lukas.bruebach@student.fhws.de>
2018-12-12 00:36:29 +0100

    Galgenmaennchen und Tik Tak Toe

--- prog1git/src/Galgenmaennchen.java
+++ prog1git/src/Galgenmaennchen.java
@@ -2,10 +2,47 @@ import java.util.Scanner;
 
 public class Galgenmaennchen {
     public static void main(String[] args){
+        System.out.println(playGame());
+    }
+    private static String returnString(){
+        String[] woerter = {"wörter", "schifffahrtskapitän", "deine", "mutter", "stinkt", "nach", "fisch", "spast", "dampfdecker", "tyrannosaurus", "dolphin", "ubuntu", "hangman", "programming", "kotlinisbetter", "thisdebug", "easteregg", "lukas", "ciri", "test20"};
+        int auswahl = (int)(Math.random()*20+1);
+        return woerter[auswahl-1];
+    }
+    private static String playGame(){
+        int versuche = 0;
         Scanner scanner = new Scanner(System.in);
-        String Antwort = "test";
-        int[] korrekteVersuche = {0,1,1,0};
-        printWord(Antwort, korrekteVersuche);
+        String Antwort = returnString();
+        int[] korrekteVersuche = new int[Antwort.length()];
+        while(versuche<16){
+            System.out.print("("+ versuche + "/15 Versuche) Bitte geben sie einen Char ein:");
+            char versuch = scanner.nextLine().charAt(0);
+            korrekteVersuche = checkAntwort(Antwort, korrekteVersuche, versuch);
+            printWord(Antwort, korrekteVersuche);
+            System.out.println();
+            boolean korrekt = true;
+            for(int i = 0; i<korrekteVersuche.length;i++){
+                if(korrekteVersuche[i]==0){
+                    korrekt = false;
+                }
+            }
+            if(korrekt){
+                return "\nDas Wort wurde erraten!";
+            }
+            versuche++;
+        }
+
+        return "Versuchslimit überschritten";
+    }
+    private static int[] checkAntwort(String Antwort, int[] korrekteVersuche, char versuch){
+        for(int i = 0; i<Antwort.length();i++){
+            if(korrekteVersuche[i]==0){
+                if (Antwort.charAt(i)==versuch){
+                    korrekteVersuche[i]=1;
+                }
+            }
+        }
+        return korrekteVersuche;
     }
     private static void printWord(String Antwort, int[] korrekteVersuche){
         for(int i = 0; i<Antwort.length(); i++){
---
+++ prog1git/src/TikTakToe.java
@@ -0,0 +1,136 @@
+import java.util.Scanner;
+
+public class TikTakToe {
+    public static void main(String[] args){
+        int[][] playfield = createPlayfield(3);
+        printPlayfield(playfield);
+        boolean noWinner = true;
+        int playerToken = 1;
+        while(noWinner){
+            playfield = playerTurn(playfield, playerToken);
+            printPlayfield(playfield);
+            playerToken++;
+            if(playerToken==3){
+                playerToken=1;
+            }
+            System.out.println(checkWinner(playfield));
+        }
+    }
+    private static int checkWinner(int[][] playfield){
+        boolean horizontalWin=false;
+        for(int row = 0; row<playfield.length; row++){
+            for (int i = 0; i < playfield.length - 1; i++) {
+                if (playfield[row][i] == playfield[row][i + 1]) {
+                    if(playfield[row][i]!=0) {
+                        horizontalWin = true;
+                    }
+                } else {
+                    horizontalWin = false;
+                }
+            }
+            if(horizontalWin){
+                return 1;
+            }
+        }
+        boolean verticalWin=false;
+        for(int column = 0; column <playfield.length; column++){
+            for (int i = 0; i < playfield.length - 1; i++) {
+                if (playfield[i][column] == playfield[i+1][column]) {
+                    if(playfield[i][column]!=0) {
+                        verticalWin = true;
+                    }
+                } else {
+                    verticalWin = false;
+                }
+            }
+            if(verticalWin){
+                return 2;
+            }
+        }
+        return 0;
+    }
+    private static int[][] playerTurn(int[][] playfield, int playerToken){
+        Scanner scanner = new Scanner(System.in);
+        String turn = scanner.nextLine();
+        String[] split = turn.split(" ");
+        boolean turnTaken = false;
+        while(!turnTaken) {
+            split = turn.split(" ");
+            if (playerToken == 1) {
+                if (playfield[Integer.parseInt(split[0])][Integer.parseInt(split[1])] == 0) {
+                    playfield[Integer.parseInt(split[0])][Integer.parseInt(split[1])] = 1;
+                    turnTaken=true;
+                } else {
+                    if (playfield[Integer.parseInt(split[0])][Integer.parseInt(split[1])] == 2) {
+                        System.out.println("Feld schon belegt!");
+                        turn = scanner.nextLine();
+                    }else{
+                        System.out.println("Feld schon belegt! (Eigenes)");
+                        turn = scanner.nextLine();
+                    }
+                }
+            }else{
+                if (playfield[Integer.parseInt(split[0])][Integer.parseInt(split[1])] == 0) {
+                    playfield[Integer.parseInt(split[0])][Integer.parseInt(split[1])] = 2;
+                    turnTaken=true;
+                } else {
+                    if (playfield[Integer.parseInt(split[0])][Integer.parseInt(split[1])] == 1) {
+                        System.out.println("Feld schon belegt!");
+                        turn = scanner.nextLine();
+                    }else{
+                        System.out.println("Feld schon belegt! (Eigenes)");
+                        turn = scanner.nextLine();
+                    }
+                }
+            }
+        }
+        return playfield;
+    }
+    private static void printPlayfield(int[][] playfield){
+        for(int i = 0; i<playfield.length;i++) {
+            for (int cnt = 0; cnt < playfield.length; cnt++) {
+                if (cnt == 0) {
+                    System.out.print("| ");
+                    if(playfield[i][cnt]==0){
+                        System.out.print(" - ");
+                    }else{
+                        if(playfield[i][cnt]==1){
+                            System.out.print(" x ");
+                        }else{
+                            System.out.print(" 0 ");
+                        }
+                    }
+                } else {
+                    if (cnt == playfield.length - 1) {
+                        if(playfield[i][cnt]==0){
+                            System.out.print(" - ");
+                        }else{
+                            if(playfield[i][cnt]==1){
+                                System.out.print(" x ");
+                            }else{
+                                System.out.print(" 0 ");
+                            }
+                        }
+                        System.out.print(" |");
+                    }else{
+                        if(playfield[i][cnt]==0){
+                            System.out.print(" - ");
+                        }else{
+                            if(playfield[i][cnt]==1){
+                                System.out.print(" x ");
+                            }else{
+                                System.out.print(" 0 ");
+                            }
+                        }
+                    }
+                }
+
+            }
+            System.out.println();
+        }
+    }
+    private static int[][] createPlayfield(int größe){
+        int[][] playfield = new int[größe][größe];
+        return playfield;
+    }
+}
